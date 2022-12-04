bf6877f0f96c1c4165f6bd919585a5d1d860deab
Achint Bhat <abhat29@asu.edu>
2022-12-03 20:47:56 -0700

    first draft

--- src/components/add-loan.component.js
+++ src/components/add-loan.component.js
@@ -4,28 +4,47 @@ import BankDataService from "../services/loan.service";
 export default class AddLoan extends Component {
     constructor(props) {
         super(props);
-        this.onChangeTitle = this.onChangeTitle.bind(this);
-        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onChangeLoanType = this.onChangeLoanType.bind(this);
+        this.onChangeLoanNumber = this.onChangeLoanNumber.bind(this);
+        this.onChangeAmount = this.onChangeAmount.bind(this);
+        this.onChangeInterest = this.onChangeInterest.bind(this);
+        this.onChangeLoanTermYears = this.onChangeLoanTermYears.bind(this);
         this.saveLoan = this.saveLoan.bind(this);
         this.newLoan = this.newLoan.bind(this);
 
         this.state = {
-            id: null,
-            title: "",
-            description: "",
-            published: false,
+            loanType: "Personal",
+            loanNumber: 1,
+            amount: 1000,
+            interest: 11,
+            loanTermYears: 0.5,
 
             submitted: false
         };
     }
 
-    onChangeTitle(e) {
+    onChangeLoanType(e) {
+        this.setState({
+            title: e.target.value
+        });
+    }
+    onChangeLoanNumber(e) {
+        this.setState({
+            title: e.target.value
+        });
+    }
+    onChangeAmount(e) {
+        this.setState({
+            title: e.target.value
+        });
+    }
+    onChangeInterest(e) {
         this.setState({
             title: e.target.value
         });
     }
 
-    onChangeDescription(e) {
+    onChangeLoanTermYears(e) {
         this.setState({
             description: e.target.value
         });
@@ -33,17 +52,21 @@ export default class AddLoan extends Component {
 
     saveLoan() {
         var data = {
-            title: this.state.title,
-            description: this.state.description
+            loanType: this.state.loanType,
+            loanNumber: this.state.loanNumber,
+            amount: this.state.amount,
+            interest: this.state.interest,
+            loanTermYears: this.state.loanTermYears
         };
-
+        console.log(data)
         BankDataService.create(data)
             .then(response => {
                 this.setState({
-                    id: response.data.id,
-                    title: response.data.title,
-                    description: response.data.description,
-                    published: response.data.published,
+                    loanType: response.data.loanType,
+                    loanNumber: response.data.loanNumber,
+                    amount: response.data.amount,
+                    interest: response.data.interest,
+                    loanTermYears: response.data.loanTermYears,
 
                     submitted: true
                 });
@@ -56,10 +79,11 @@ export default class AddLoan extends Component {
 
     newLoan() {
         this.setState({
-            id: null,
-            title: "",
-            description: "",
-            published: false,
+            loanType: "Personal",
+            loanNumber: 1,
+            amount: 1000,
+            interest: 11,
+            loanTermYears: 0.5,
 
             submitted: false
         });
@@ -69,39 +93,76 @@ export default class AddLoan extends Component {
             <div className="submit-form">
                 {this.state.submitted ? (
                     <div>
-                        <h4>You submitted successfully!</h4>
+                        <h4>Data submitted successfully!</h4>
                         <button className="btn btn-success" onClick={this.newLoan}>
-                            Add
+                            Add another Loan
                         </button>
                     </div>
                 ) : (
                     <div>
                         <div className="form-group">
-                            <label htmlFor="title">Title</label>
+                            <label htmlFor="loanType">Loan Type</label>
                             <input
                                 type="text"
                                 className="form-control"
-                                id="title"
+                                id="loanType"
                                 required
-                                value={this.state.title}
-                                onChange={this.onChangeTitle}
-                                name="title"
+                                value={this.state.loanType}
+                                onChange={this.onLoanType}
+                                name="loanType"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="loanNumber">Loan Number</label>
+                            <input
+                                type="number"
+                                className="form-control"
+                                id="loanNumber"
+                                required
+                                value={this.state.loanNumber}
+                                onChange={this.onChangeLoanNumber}
+                                name="loanNumber"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="amount">Amount</label>
+                            <input
+                                type="number"
+                                className="form-control"
+                                id="amount"
+                                required
+                                value={this.state.amount}
+                                onChange={this.onChangeAmount}
+                                name="amount"
                             />
                         </div>
 
                         <div className="form-group">
-                            <label htmlFor="description">Description</label>
+                            <label htmlFor="interest">Interest</label>
                             <input
-                                type="text"
+                                type="number"
                                 className="form-control"
-                                id="description"
+                                id="interest"
                                 required
-                                value={this.state.description}
-                                onChange={this.onChangeDescription}
-                                name="description"
+                                value={this.state.interest}
+                                onChange={this.onChangeInterest}
+                                name="interest"
                             />
                         </div>
 
+                        <div className="form-group">
+                            <label htmlFor="description">Loan Term (Years)</label>
+                            <input
+                                type="number"
+                                className="form-control"
+                                id="loanTermYears"
+                                required
+                                value={this.state.loanTermYears}
+                                onChange={this.onChangeLoanTermYears}
+                                name="loanTermYears"
+                            />
+                        </div>
+                        <br/>
                         <button onClick={this.saveLoan} className="btn btn-success">
                             Submit
                         </button>
